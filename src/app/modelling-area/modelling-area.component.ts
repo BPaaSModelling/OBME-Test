d8281a18b6b1563e32270907c33693440db5cc1c
charutapande <32811202+charutapande@users.noreply.github.com>
2017-12-08 18:48:05 +0100

    Change shape to ellipse and define id for div

--- src/app/modelling-area/modelling-area.component.ts
+++ src/app/modelling-area/modelling-area.component.ts
@@ -40,45 +40,6 @@ console.log('Constructor of graph');
       min: 0.1,
       max: 1.5
     };
-
-    /*this.style = this.style || cytoscape.stylesheet()
-      .selector('node')
-      .css({
-        'content': 'data(name)',
-        'shape': 'rectangle',
-        'text-valign': 'center',
-        'background-color': 'data(faveColor)',
-        'width': '200px',
-        'height': '100px',
-        'color': 'black'
-      })
-      .selector(':selected')
-      .css({
-        'border-width': 3,
-        'border-color': '#333'
-      })
-      .selector('edge')
-      .css({
-        'label': 'data(label)',
-        'color': 'black',
-        'curve-style': 'bezier',
-        'opacity': 0.666,
-        'width': 'mapData(strength, 70, 100, 2, 6)',
-        'target-arrow-shape': 'triangle',
-        'line-color': 'data(faveColor)',
-        'source-arrow-color': 'data(faveColor)',
-        'target-arrow-color': 'data(faveColor)'
-      })
-      .selector('edge.questionable')
-      .css({
-        'line-style': 'dotted',
-        'target-arrow-shape': 'diamond'
-      })
-      .selector('.faded')
-      .css({
-        'opacity': 0.25,
-        'text-opacity': 0
-      });*/
   }
 
   ngOnInit() {
